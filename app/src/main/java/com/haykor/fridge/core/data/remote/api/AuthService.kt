e65303dd037aead29f51a8b258b689c522836ebf
Arthur <baginian.artur@yandex.ru>
2025-11-13 20:45:58 +0300

    feat(api): create auth & user service interfaces

--- app/src/main/java/com/haykor/fridge/core/data/remote/api/AuthService.kt
+++ app/src/main/java/com/haykor/fridge/core/data/remote/api/AuthService.kt
@@ -1,4 +1,25 @@
 package com.haykor.fridge.core.data.remote.api
 
+import com.haykor.fridge.core.data.remote.models.AuthResponse
+import com.haykor.fridge.core.data.remote.models.LoginRequest
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface AuthService {
+    @POST("auth/login")
+    suspend fun login(
+        @Body request: LoginRequest,
+        @Header("user-agent") userAgent: String
+    ): AuthResponse
+
+    @POST("auth/refresh_tokens")
+    suspend fun refreshTokens(
+        @Header("User-Agent") userAgent: String,
+        @Header("Cookie") refreshToken: String,
+    ): AuthResponse
+
+    @POST("auth/logout")
+    suspend fun logout()
 }
+
--- app/src/main/java/com/haykor/fridge/core/data/remote/api/UserService.kt
+++ app/src/main/java/com/haykor/fridge/core/data/remote/api/UserService.kt
@@ -1,2 +1,10 @@
-package com.haykor.fridge.core.data.remote.api 
+package com.haykor.fridge.core.data.remote.api
 
+import com.haykor.fridge.core.data.remote.models.CreateUserRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserService {
+    @POST("users/")
+    suspend fun createUser(@Body request: CreateUserRequest)
+}
